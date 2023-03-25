dd68613bea544b7c4e8584f52baa59d7c630d8b7
wakefullynx <mail@wakefullynx.dev>
2023-03-25 03:14:18 +0100

    Explicitly set exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,5 @@
-pub mod smallest_enclosing_circle;
+mod smallest_enclosing_circle;
+
+pub use self::smallest_enclosing_circle::smallest_enclosing_circle;
+pub use self::smallest_enclosing_circle::smallest_enclosing_circle_recursive;
+pub use self::smallest_enclosing_circle::Circle;
