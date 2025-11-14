b4584e6d8e3171d483944ad2706420bc324dcd02
philippebner <29914922+philippebner@users.noreply.github.com>
2025-11-14 09:16:33 +0100

    Add crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,30 @@
-//! Iterative and recursive two-dimensional implementations of Welzl's algorithm for computing the smallest enclosing circle.
-//!
-//! The implementations in this crate solve the smallest enclosing circle problem, also known as smallest-circle problem, minimum covering circle problem, or bounding circle problem.
-//! Welzl's algorithm solves this problem in expected O(n) runtime.
-//! The original algorithm was formulated as recursive program, which leads to call stack overflow for larger problem sizes.
-//! Thus, the iterative implementation in this crate should be preferred.
-//! However, the recursive version is provided for demonstration purposes.
-//!
-//!
-//! The implementation is based on the following work:
-//!
-//! Welzl, E. (1991). Smallest enclosing disks (balls and ellipsoids).
-//! In New results and new trends in computer science (pp. 359-370).
-//! Springer, Berlin, Heidelberg.
-//! 
-//! # A Note on Custom Predicates
-//!
-//! # Examples
-//!
-//! ```
-//! use smallest_enclosing_circle::smallest_enclosing_circle;
-//!
-//! // Input: Four corner points of square box of unit size
-//! let points = Vec::from([[0., 0.], [1., 0.], [1., 1.], [0., 1.]]);
-//! let circle = smallest_enclosing_circle(points.into_iter());
-//! println!("Circle: {:?}", circle);
-//! // Circle: Three([0.0, 1.0], [1.0, 1.0], [1.0, 0.0], false);
-//! println!("Center: {:?}", circle.center());
-//! // Center: Some([0.5, 0.5])
-//! println!("Radius: {:?}", circle.radius());
-//! // Radius: 0.7071067811865476
-//! ```
+/// Takes an iterator over two-dimensional points and returns the smallest [Circle] that encloses all points.
+///
+/// Iterative version of Welzl's algorithm, which was originally formulated as recursive algorithm.
+/// The expected input is an of [f64; 2] coordinate pairs with actual numbers (no NaNs or Infinites). Duplicates are allowed.
+/// Note that the original algorithm is based on randomizing the order of input points.
+/// This is omitted in this crate, however randomization can be done by the caller in advance.
+/// The advantage over the recursive algorithm is that large problem sizes do not run into call stack problems.
+/// The result is a [`Circle2D`] enum.
+///
+/// The implementation is based on the following work:
+///
+/// Welzl, E. (1991). Smallest enclosing disks (balls and ellipsoids).
+/// In New results and new trends in computer science (pp. 359-370).
+/// Springer, Berlin, Heidelberg.
+///
+///
+/// # Examples
+///
+/// ```
+/// use smallest_enclosing_circle::smallest_enclosing_circle;
+/// use smallest_enclosing_circle::predicates::in_circle::DefaultInCircle;
+///
+/// // Input: Four corner points of square box of unit size
+/// let circle = smallest_enclosing_circle([[0., 0.], [1., 0.], [1., 1.], [0., 1.]]);
+/// assert_eq!(circle.center(), Some([0.5, 0.5]));
+/// assert_eq!(circle.radius(), Some(f64::sqrt(2.) / 2.));
+/// ```
 
 pub mod algorithm;
 pub mod circle;
